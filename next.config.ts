cf07a08b6f61157f3f2624bb1e95c53f1578bd1d
Do Nguyen Tung Duong <furyghost07@gmail.com>
2025-08-12 11:09:30 +0700

    Add output configuration to Next.js settings

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
+  output: 'export',
   basePath: isProd ? '/ImageCroper' : '', assetPrefix: isProd ? '/ImageCroper/' : '', images: {
     unoptimized: true, // GitHub Pages does not support Next.js image optimization }, 
   },
